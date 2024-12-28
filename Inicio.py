5e21143e98305e6ab7b398b70ac248483dfd2b14
jbolanyos <jbolanyos@gmail.com>
2024-12-28 15:18:18 -0600

    actualizacion 28 de diciembre 15:18

--- Inicio.py
+++ Inicio.py
@@ -5,3 +5,13 @@ st.title("Proyecto Final")
 st.header("FIFA PLAYERS")
 st.image("utils/1241.jpg")
 
+st.html("<h3><b>Proyecto FIFA: Generar información valiosa a partir de datos de fútbol de todo el mundo.</b></h3>")
+st.html("<h3><b>El fútbol es el deporte rey, seguido por millones de personas a nivel mundial. Genera mucha actividad comercial y cultural en muchos países en donde se practica, así como una cantidad enorme de datos.</b></h3>")
+st.html("<h3><b>El presente proyecto es una muestra de las posibilidades en que pueden aprovecharse los datos en el fútbol:</b></h3>")
+st.html("<h3><b>-Los aficionados pueden incrementar su conocimiento de su equipo y jugadores favoritos, así como también adelantarse a predecir los resultados de los partidos.</b></h3>")
+st.html("<h3><b>-Las marcas pueden verificar los equipos y jugadores más populares y verificar qué eventos deportivos pueden generar mayores ingresos.</b></h3>")
+st.html("<h3><b>-Los entrenadores y directivos de clubes pueden conocer características competitivas de los jugadores, conocer a sus competidores y crear estrategias que les permitan sobresalir de los demás equipos o clubes.</b></h3>")
+st.html("<h3><b>-Los periodistas deportivos podrán identificar tendencias en los resultados, dar seguimiento a equipos y jugadores específicos para caracterizarlos, analizar los resultados d ellos partidos o predecirlos.</b></h3>")
+
+
+
--- pages/4_⚽_CONCLUSIONES.py
+++ pages/4_⚽_CONCLUSIONES.py
@@ -37,3 +37,5 @@ with col1:
     st.subheader("6:")
 with col2:
     st.text("El modelo de aprendizaje supervisado permite determinar el grado de influencia entre diversas variables de los datos de los jugadores, como por ejemplo, determinar qué tanto influye la velocidad de un jugador en su resistencia en la cancha.")
+
+st.html("<h2><b>Esta ha sido una pequeña muestra de las posibilidades de convertir los datos del fútbol en oportunidades. El llamado es a desarrollar proyectos locales que incentiven a los interesados en utilizar esta información como apoyo en sus decisiones o iniciativas.</b></h2>")
